c5788e510772eaf9546a520af98656fedbf15622
Dream0fme <sahaheh@mail.ru>
2021-06-28 18:54:25 +0800

    add Data Binding for SplashScreen

--- app/src/main/java/urum/geoplanner/ui/ActivityHome.java
+++ app/src/main/java/urum/geoplanner/ui/ActivityHome.java
@@ -5,11 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import urum.geoplanner.R;
+import urum.geoplanner.databinding.ActivityHomeBinding;
 import urum.geoplanner.service.ConnectorService;
 
 
@@ -17,14 +16,15 @@ public class ActivityHome extends AppCompatActivity {
     int SPLASH_TIME;
     int SPLASH_TIME_MIN = 1200;
     int SPLASH_TIME_MAX = 3000;
-    ProgressBar splashProgress;
+
+    ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        splashProgress = findViewById(R.id.splashProgress);
         SPLASH_TIME = rnd(SPLASH_TIME_MIN, SPLASH_TIME_MAX);
         /*splashProgress.getProgressDrawable().setColorFilter(
                 getResources().getColor(R.color.colorAccent), PorterDuff.Mode.SRC_IN);*/
@@ -32,6 +32,7 @@ public class ActivityHome extends AppCompatActivity {
         Log.d("mytag", "splash time: " + SPLASH_TIME);
         ConnectorService connectorService = new ConnectorService(this);
         getLifecycle().addObserver(connectorService);
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -49,7 +50,7 @@ public class ActivityHome extends AppCompatActivity {
     }
 
     private void playProgress() {
-        ObjectAnimator.ofInt(splashProgress, "progress", 100)
+        ObjectAnimator.ofInt(binding.splashProgress, "progress", 100)
                 .setDuration(SPLASH_TIME)
                 .start();
     }
