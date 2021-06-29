c77005d3c433e08a71210624b744449185515c2a
Dream0fme <sahaheh@mail.ru>
2021-06-29 16:26:45 +0800

    formatted lines

--- app/src/main/java/urum/geoplanner/ui/MainActivity.java
+++ app/src/main/java/urum/geoplanner/ui/MainActivity.java
@@ -44,7 +44,6 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResponse;
 import com.google.android.gms.location.SettingsClient;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
